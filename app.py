672d694d77a6742f9c2a1e162f957336d63fd108
aryanusadi <121725403+aryanusadi@users.noreply.github.com>
2025-05-02 18:18:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,30 +30,30 @@ def main():
 
 def run_ml_app():
     design = """<div style="padding:15px;">
-                    <h1 style="color:#fff">Loan Eligibility Prediction</h1>
+                    <h1 style="color:#000">Loan Eligibility Prediction</h1>
+                    <p style="color:#000">Fill this simulation according to the actual condition</p>
                 </div
              """
     st.markdown(design, unsafe_allow_html=True)
-    left,right = st.columns((2.2))
-    gender = left.selectbox('Gender',('Male','Female'))
-    married = right.selectbox('Married',('Yes','No'))
-    dependent = left.selectbox('Dependents',('None','One','Two','Three'))
-    education = right.selectbox('Education',('Graduate','Non-Graduate'))
-    self_employed = left.selectbox('Self-Employed',('Yes','No'))
-    applicant_income = right.selectbox('Applicant Income')
-    coApplicant_income = left.selectbox('Co-Applicant Income')
-    loan_amount = right.selectbox('Loan Amount')
-    loan_amount_term = left.selectbox('Loan Tenor in Days')
-    credit_history = right.selectbox('Credit History',(0.0 , 1.0))
-    property_area = st.selectbox('Property Area',('Urban','Semiurban','Rural'))
+    left,right = st.columns((2,2))
+    gender = left.selectbox('Gender',('Male', 'Female')) 
+    married = right.selectbox(('Married'),('Yes', 'No')) 
+    dependent = left.selectbox('Dependents',('None', 'One', 'Two', 'Three'))
+    education = right.selectbox(('Education'),('Graduate', 'Non-Graduate')) 
+    self_employed = left.selectbox('Self-Employed',('Yes', 'No')) 
+    applicant_income = right.number_input('Apllicant Income')
+    coApplicant_income = left.number_input('Co-Apllicant Income')
+    loan_amount =  right.number_input('Loan Amount')
+    loan_amount_term = left.number_input('Loan Tenor in Days')
+    credit_history = right.selectbox(('Credit History'),(0.0, 1.0))
+    property_area = st.selectbox('Property Area', ('Semiurban','Urban', 'Rural'))
     button = st.button("Predict")
-    
 
     #If button is clilcked
     if button:
         result = predict(gender, married, dependent, education, self_employed, applicant_income, coApplicant_income
                          ,loan_amount, loan_amount_term, credit_history, property_area)
-        if result == 'eligible':
+        if result == 'Eligible':
             st.success(f'You are {result} for the loan')
         else:
             st.warning(f'You are {result} for the loan')
@@ -63,21 +63,18 @@ def predict(gender, married, dependent, education, self_employed, applicant_inco
     
     #Making prediction (process user input)
     gen = 0 if gender == 'Male' else 1
-    mar = 0 if gender == 'Yes'  else 1
-    dep = float(0 if dependent == 'None'
-                else 1 if dependent == 'One'
-                else 2 if dependent == 'Two'
-                else 3)
+    mar = 0 if married == 'Yes' else 1
+    dep = float(0 if dependent == 'None' else 1 if dependent == 'One' else 2 if dependent == 'Two' else 3)
     edu = 0 if education == 'Graduate' else 1
     sem = 0 if self_employed == 'Yes' else 1
-    pro = 0 if property_area == 'Semiurban' else 1 if property_area == 'Urban'else 2
+    pro = 0 if property_area == 'Semiurban' else 1 if property_area == 'Urban' else 2
     lam = loan_amount/1000
     cap = coApplicant_income/1000
 
-    prediction = Logistic_Regression_Model.predict ([[
-         gen, mar, dep, edu, sem, applicant_income, cap, lam, loan_amount_term, credit_history,pro
+    prediction = Logistic_Regression_Model.predict([[
+        gen, mar, dep, edu, sem, applicant_income, cap, lam, loan_amount_term, credit_history, pro
     ]])
-   
+    
     result = "Not Eligible" if prediction == 0 else "Eligible"
     return result
 
